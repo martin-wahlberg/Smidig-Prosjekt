5cb8f7baea0872ee0a747b5c2d83655071067df0
wahmar <wahmar16@student.westerdals.no>
2017-11-30 12:22:16 +0100

    Nytt

---
+++ js/indexScript.js
@@ -0,0 +1,14 @@
+function onLoadFunctions(){
+  setSize();
+}
+function setSize(){
+  var clientHeight = document.getElementById('gCS').clientHeight;
+  var clientWidth = document.getElementById('gCS').clientWidth;
+  document.getElementById("gameContainer").style.height = clientHeight;
+  document.getElementById("gameContainer").style.width = clientWidth;
+}
+
+
+function gameWinTrue(){
+document.getElementById("conTainer")style.backgroundColor = "green";
+}
