639083fbcf1f4bbbe0f0cc11dbd6f644b9672eb1
wahmar <wahmar16@student.westerdals.no>
2018-05-26 03:10:14 +0200

    Duplikater fjernet

--- pages/getCoupons.php
+++ pages/getCoupons.php
@@ -1,5 +1,6 @@
 <?php
 $t = 0;
+$a=array();
 echo '{"tilbud":[';
 include('connection.php');
 $userEmail = $_GET['mail'];
@@ -13,22 +14,13 @@ if ($conn->connect_error) {
 $sql = "SELECT kupongId FROM kuponger WHERE mailC = '$userEmail'";
 $result = $conn->query($sql);
 
+//Her blir det sjekket om alle kupponger er innafor 2 ganger så det blir sendt ut uansett om den er brukt siden de brukte blir sjekket først så blir de ikke husket.
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
     $kupongId = $row["kupongId"];
-    $sql = "SELECT tilbud, id, fordel, imgurl FROM tilbud WHERE NOT id = '$kupongId'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo '{ "tilbud":"'. $row["tilbud"]. '" , "id":"'. $row["id"]. '" , "fordel":"'. $row["fordel"]. '" , "imgurl":"'. $row["imgurl"]. '" },';
-        $t++;
-    }
-} else {
-    echo "0 resultas";
-}
+    array_push($a, "$kupongId");
+    $something = "done";
     }
 } else {
   $sql = "SELECT tilbud, id, fordel, imgurl FROM tilbud";
@@ -44,6 +36,29 @@ if ($result->num_rows > 0) {
   echo "0 results";
 }
 }
+if (isset($something)) {
+   $sql = "SELECT tilbud, id, fordel, imgurl FROM tilbud WHERE NOT id = '$x_value'";
+   $result = $conn->query($sql);
+
+   if ($result->num_rows > 0) {
+       // output data of each row
+       while($row = $result->fetch_assoc()) {
+         $chickChack = "false";
+         foreach($a as $x => $x_value) {
+           if ($row["id"] === $x_value) {
+             $chickChack = "true";
+           }
+         }
+         if ($chickChack ==="false") {
+           echo '{ "tilbud":"'. $row["tilbud"]. '" , "id":"'. $row["id"]. '" , "fordel":"'. $row["fordel"]. '" , "imgurl":"'. $row["imgurl"]. '" },';
+         }
+       }
+   } else {
+
+   }
+
+}
+
 $conn->close();
 echo "{}]}";
 ?>
